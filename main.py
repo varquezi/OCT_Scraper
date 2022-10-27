81ccbb1582df56823c37161f73fccbbd9d5545ef
varquezi <ivangvarquez@gmail.com>
2022-10-27 00:38:31 -0400

    Added .gitignore for __pycache__

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import teacherScraper
 
 if __name__ == "__main__":
-    teacherScraper.getTeachers(678300 , 678400, 'f41uk_O3QGF1Mo0.tf_sTdl_EUedR6pksz')
+    teacherScraper.getTeachers(678300 , 678350, 'f41uk_O3QGF1Mo0.tf_sTdl_EUedR6pksz')
